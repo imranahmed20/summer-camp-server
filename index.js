5f990084cdb5686556696898e3d345a68d0d16ce
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-06-10 01:20:55 +0600

    delete add classes commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9qf7kmv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -78,6 +78,14 @@ async function run() {
             const item = req.body;
             const result = addClassCollection.insertOne(item)
             res.send(result)
+        })
+
+
+        app.delete('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await addClassCollection.deleteOne(query)
+            res.send()
         })
 
 
