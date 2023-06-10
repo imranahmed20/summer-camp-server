136f864dbd98f26967ce8eb765f7806d518a40f7
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-06-11 01:44:06 +0600

    security added admin

--- index.js
+++ index.js
@@ -83,6 +83,19 @@ async function run() {
             res.send(result)
         })
 
+        // security added
+
+        app.get('/users/admin/:id', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const result = { admin: user?.role == "admin" }
+            res.send(result)
+        })
+
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
